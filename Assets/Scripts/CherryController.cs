0c01d3f2509728657d69819ae7571dff71bbd81f
Reagan Brasch <reaganbrasch@gmail.com>
2021-11-05 18:16:18 +1100

    implemented cherry to move across the screen from a random side of the level and at a random point

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -4,33 +4,75 @@ using UnityEngine;
 
 public class CherryController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Tweener tweener;
+
+    private float half_x, half_y;
+    private bool spawn = true;
+
     void Start()
     {
+        tweener = GetComponent<Tweener>();
+
+        Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        half_x = camera.aspect * camera.orthographicSize + 0.25f;
+        half_y = camera.orthographicSize + 0.25f;
+
         StartCoroutine(test());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        randomLocation();
+        /*
+        if (spawn)
+        {
+            spawn = false;
+            Debug.Log("boi");
+            StartCoroutine(SpawnCherry());
+        }
+
+        Debug.Log(tweener.waiting());
+        //if (tweener.waiting())
+            //spawn = true;
+        */
     }
 
     private IEnumerator test()
     {
-        //while (true)
-
         while (true)
         {
             yield return new WaitForSeconds(10f);
-            Debug.Log("spawn cherry");
+
+            randomLocation();
+
+            // offset the tween time
+            yield return new WaitForSeconds(20f);
+
+            yield return null;
         }
+    }
+
+    private IEnumerator SpawnCherry()
+    {
+        yield return new WaitForSeconds(10f);
+        randomLocation();
     }
 
     private void randomLocation()
     {
+        // 0: U, 1: R, 2: D, 3: L
         int side = Random.Range(0, 4);
-        Debug.Log(side);
-        //transform.position
+        float new_x = Random.Range(-half_x, half_x);
+        float new_y = Random.Range(-half_y, half_y);
+
+        switch (side)
+        {
+            case 0: transform.position = new Vector3(new_x, half_y, 1); break;
+            case 1: transform.position = new Vector3(half_x, new_y, 1); break;
+            case 2: transform.position = new Vector3(new_x, -half_y, 1); break;
+            case 3: transform.position = new Vector3(-half_x, new_y, 1); break;
+        }
+
+        Vector3 endPoint = Vector3.zero - transform.position;
+        tweener.AddTween(transform, transform.position, endPoint, 20f);
     }
 }
